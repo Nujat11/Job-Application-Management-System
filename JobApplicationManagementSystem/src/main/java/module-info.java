49d4690f2729ed00612f8f758fd767464fdaa3c3
Nujat-E- Hasnat <nujatehasnat11@gmail.com>
2025-05-03 08:35:26 +0600

    Delete JobApplicationManagementSystem directory

--- JobApplicationManagementSystem/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.jobapplicationmanagementsystem {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.jobapplicationmanagementsystem to javafx.fxml;
-    exports com.example.jobapplicationmanagementsystem;
-}
